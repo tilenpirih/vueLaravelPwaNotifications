19906c486be81c6febb76575259800be8884be8e
Tilen Pirih <tilen.pirih@gmail.com>
2025-08-21 13:33:41 +0200

    set up API routing and remove web routes; add initial API response

--- be/routes/web.php
+++ be/routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// All web routes removed. Use routes/api.php for API endpoints with /api prefix.
--- be/tests/Feature/ExampleTest.php
+++ be/tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
 test('the application returns a successful response', function () {
-    $response = $this->get('/');
+    $response = $this->get('/api');
 
     $response->assertStatus(200);
 });
