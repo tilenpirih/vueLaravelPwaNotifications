d2953f82da1a168e134033d3a4f9253b80d5c317
Tilen Pirih <tilen.pirih@gmail.com>
2025-08-22 22:22:17 +0200

    Create empty laravel project

--- be/routes/web.php
+++ be/routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// All web routes removed. Use routes/api.php for API endpoints with /api prefix.
+Route::get('/', function () {
+    return view('welcome');
+});
